b303c9507ec115da52fd17c1cab2acc5ee05adaa
Ekaterina Dronova <katedronova96@gmail.com>
2022-11-14 18:47:29 +0300

    feat: add function chaining

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ Builder.prototype.plus = function(...n){
   for (const nSingle of this.n) {
     this.x += nSingle;
   }
-  return this.x;
+  return this;
 };
 
 Builder.prototype.minus = function(...n){
@@ -17,22 +17,25 @@ Builder.prototype.minus = function(...n){
   for (const nSingle of this.n) {
     this.x -= nSingle;
   }
-  return this.x;
+  return this;
 };
 
 Builder.prototype.multiply = function(y){
   this.y = y;
-  return this.x * this.y;
+  this.x *= this.y;
+  return this;
 };
 
 Builder.prototype.divide = function(y){
   this.y = y;
-  return this.x / this.y;
+  this.x /= this.y;
+  return this;
 };
 
 Builder.prototype.get = function(){
-  return this.x;
+  return this;
 };
+
 
 /////// ES6 for numbers
 class IntBuilder extends Builder {
@@ -43,7 +46,8 @@ class IntBuilder extends Builder {
 
   mod(y) {
     this.y = y;
-    return this.x % this.y;
+    this.x = this.x % this.y;
+    return this;
   }
 
   static random(from, to) {
@@ -53,18 +57,33 @@ class IntBuilder extends Builder {
   }
 }
 
+/////// Check 1
+
+// let intBuilder = new IntBuilder(10); // 10;
+// intBuilder
+//   .plus(2, 3, 2)                     // 17;
+//   .minus(1, 2)                       // 14;
+//   .multiply(2)                       // 28;
+//   .divide(4)                         // 7;
+//   .mod(3)                            // 1;
+//   .get();                            // -> 1;
+// console.log(intBuilder.x);
+// console.log(IntBuilder.random(10, 100));
+
+
 /////// ES5 for strings
 function StringBuilder(x) {
   Builder.call(this, x);
   this.x = x||'';
 }
+
 StringBuilder.prototype = Object.create(Builder.prototype);
-//can also set through Object.setPrototypeOf(StringBuilder, Builder) ?;
 
 StringBuilder.prototype.sub = function(from, n) {
   this.from = from;
   this.n = n;
-  return this.x.substr(this.from, this.n);
+  this.x = this.x.substr(this.from, this.n);
+  return this;
 }
 
 StringBuilder.prototype.remove = function(str) {
@@ -74,19 +93,36 @@ StringBuilder.prototype.remove = function(str) {
   if (from == -1) {
     return;
   } else {
-    return `${this.x.slice(0, from)}${this.x.slice(to)}`;
+    this.x = `${this.x.slice(0, from)}${this.x.slice(to)}`;
+    return this;
   }
 }
 
 StringBuilder.prototype.minus = function(y) {
-  return this.x.replace(this.x.slice(-y),'');
+  this.x = this.x.replace(this.x.slice(-y),'');
+  return this;
 }
 
 StringBuilder.prototype.multiply = function(y) {
-  return this.x.repeat(y);
+  this.x = this.x.repeat(y);
+  return this;
 }
 
 StringBuilder.prototype.divide = function(y) {
   let k = Math.floor(this.x.length /y);
-  return this.x.substr(0, k);
+  this.x = this.x.substr(0, k);
+  return this;
 }
+
+/////// Check 2
+
+// let strBuilder = new StringBuilder('Hello'); // 'Hello';
+// strBuilder
+//   .plus(' all', '!')                         // 'Hello all!'
+//   .minus(4)                                  // 'Hello '
+//   .multiply(3)                               // 'Hello Hello Hello '
+//   .divide(4)                                 // 'Hell';
+//   .remove('ll')                               // 'He';
+//   .sub(1,1)                                  // 'e';
+//   .get();                                    // -> 'e';
+// console.log(strBuilder.x);
